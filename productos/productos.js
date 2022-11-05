ab0b6e121e5096e74644eca0f49af4a1935417b4
ehernandep <65094614+ehernandep@users.noreply.github.com>
2022-11-05 12:19:34 -0500

    prueba 1

---
+++ productos/productos.js
@@ -0,0 +1,92 @@
+export const productos = [
+  {
+    id: 1,
+    title: 'MAXILLA-R BLUSH MULTI',
+    price: 120.0,
+    color: 'salmon',
+    stock: 9,
+    imagen:
+      'http://cdn.shopify.com/s/files/1/0241/9654/6624/products/STEVEMADDEN-SHOES_MAXIMA_BLUSH_INSIDE_preview_1_grande.jpg?v=1619196733',
+  },
+  {
+    id: 2,
+    title: 'POSSESSION BLACK/TAN',
+    price: 99.95,
+    color: 'black-tan',
+    stock: 2,
+    imagen:
+      'https://cdn.shopify.com/s/files/1/2170/8465/products/STEVEMADDEN_SHOES_POSESSION_BLACK-TAN_SIDE.jpg?v=1635426424',
+  },
+  {
+    id: 3,
+    title: 'POSSESSION BLACK',
+    price: 99.95,
+    color: 'black',
+    stock: 1,
+    imagen:
+      'https://cdn.shopify.com/s/files/1/2170/8465/products/STEVEMADDEN-SHOES_DRAGON_BLACK_SIDE-1.jpg?v=1626209267',
+  },
+  {
+    id: 4,
+    title: 'CATCHER WHITE BLACK',
+    price: 74.96,
+    color: 'white-black',
+    stock: 15,
+    imagen:
+      'https://cdn.shopify.com/s/files/1/2170/8465/products/STEVEMADDEN-SNEAKER_CHARLIE_WHITE-BLACK_SIDE.jpg?v=1587667835',
+  },
+  {
+    id: 5,
+    title: 'CHARLIZE BLACK',
+    price: 150.9,
+    color: 'black',
+    stock: 5,
+    imagen:
+      'http://cdn.shopify.com/s/files/1/0241/9654/6624/products/STEVEMADDEN_SHOES_CHARLIZE_BLACK-SATIN_SIDE_preview_grande.jpg?v=1658757865',
+  },
+  {
+    id: 6,
+    title: 'AMULET BLACK',
+    price: 120.0,
+    color: 'black',
+    stock: 2,
+    imagen:
+      'http://cdn.shopify.com/s/files/1/0241/9654/6624/products/amulet-black_jpg_grande.jpg?v=1658330574',
+  },
+  {
+    id: 7,
+    title: 'RIBETRINI',
+    price: 110.95,
+    color: 'black',
+    stock: 0,
+    imagen:
+      'https://cdn.shopify.com/s/files/1/0269/9388/5267/products/STEVEMADDEN-SHOES_CYPRESS_BLACK_SIDE_500x500.jpg?v=1660257319',
+  },
+  {
+    id: 8,
+    title: 'APPROACH BLACK',
+    price: 90.0,
+    color: 'black',
+    stock: 3,
+    imagen:
+      'https://cdn.shopify.com/s/files/1/2170/8465/products/STEVEMADDEN-SHOES_LANDO_BLACK-LEATHER_SIDE_300x.jpg?v=1633118586',
+  },
+  {
+    id: 9,
+    title: 'VALA BLACK/BRONZE',
+    price: 100.0,
+    color: 'black',
+    stock: 6,
+    imagen:
+      'http://cdn.shopify.com/s/files/1/0241/9654/6624/products/STEVEMADDEN-INTL_VALA-SN_BLACK-BRONZE_SIDE_preview_grande.jpg?v=1655707319',
+  },
+  {
+    id: 10,
+    title: 'POSSESSION BRIGHT MULTI',
+    price: 99.95,
+    color: 'multicolor',
+    stock: 7,
+    imagen:
+      'https://cdn.shopify.com/s/files/1/2170/8465/products/STEVEMADDEN_SHOES_POSSESSION_BRIGHT-MULTI_SIDE.jpg?v=1639495927',
+  },
+];
