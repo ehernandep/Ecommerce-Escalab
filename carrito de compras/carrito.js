712f50b4d0c7737172e72fe15f926326822458e3
unknown <manualvarezc2245@gmail.com>
2022-11-08 21:20:36 -0500

    metodosPago

--- carrito de compras/carrito.js
+++ carrito de compras/carrito.js
@@ -2,11 +2,19 @@ let cart=document.querySelector("#abrirCarrito")
 let abierto=document.getElementById("cart-icon");
 let cerrado= document.getElementById("cerrarCarrito");
 
-let borrarTennis=document.getElementById("cart-remove")
+let borrarTennis=document.getElementById("cart-remove");
+
+let buyNow=document.querySelector(".btn-buy")
+let pago=document.querySelector(".pago")
+
+const comprar=()=>{
+  pago.classList.add("pagoclick");
+}
 
 const abrirCart = () => {
   cart.classList.add("open");
   //console.log("ya!")
+  pago.classList.remove("pagoclick");
 };
 abierto.addEventListener("click",abrirCart);
 
@@ -15,13 +23,10 @@ const cerrarCarrito = () => {
   cart.classList.remove("open")
   //console.log("no")
 };
+
 cerrado.addEventListener("click",cerrarCarrito);
 
 const añadirCarrito = () => {
  /*  for(let i=0; i<) */
   console.log("hola")
 };
-
-const borrarCarrito=()=>{
-
-}
