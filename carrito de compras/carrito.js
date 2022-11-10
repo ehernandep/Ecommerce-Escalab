dd39cc2ec81ee57d066a3135bae7ef4a7d74b4a5
ehernandep <65094614+ehernandep@users.noreply.github.com>
2022-11-09 20:20:44 -0500

    mostrarProductosYElmininar

--- carrito de compras/carrito.js
+++ carrito de compras/carrito.js
@@ -17,6 +17,20 @@ const abrirCart = () => {
   // solo los llama cuando se abre el carrito
   const objetosCarrito = JSON.parse(localStorage.getItem('item'));
   console.log(objetosCarrito);
+  const showproducts = () => {
+    let cartshoes = '';
+    objetosCarrito.forEach((zapato) => {
+      cartshoes += `<div class="d-flex justify-content-center"><img class="cart-img" src=${zapato.img} /></div>
+        <div class="details d-flex justify-content-center">
+          <div class="product-title">${zapato.title}</div>
+          <div class="product-price">Precio: ${zapato.price}</div>
+          <input type="number" value="1" class="quantity" />
+          <button class="btn bx bxs-trash cart-remove" id="cart-remove" onclick="borrarCarrito('${zapato.title}')"></button>
+        </div>`;
+    });
+    document.getElementById('cart-content').innerHTML = cartshoes;
+  };
+  showproducts();
 };
 abierto.addEventListener('click', abrirCart);
 
@@ -30,11 +44,20 @@ cerrado.addEventListener('click', cerrarCarrito);
 const añadirCarrito = (title, price, img) => {
   // cree un objeto para que se vea todo mas bonito y para posterirormente hacer el display mas facilmente dentro del carrito
   const guardarItemEnCarrito = { title: title, price: price, img: img };
-  // arriba defini un array vacio aqui simplemente con cada click añado al array 
+  // arriba defini un array vacio aqui simplemente con cada click añado al array
   arrayObjetosGuardados.push(guardarItemEnCarrito);
   // aca le denomino el item al local storage y lo llame item, de segundo esta el valor de los objetos guardados
   // cuando uno guarda los datos debe usar stringify para que quede bien luego hace parse
   localStorage.setItem('item', JSON.stringify(arrayObjetosGuardados));
+};
+const borrarCarrito = (zapatoBorrar) => {
+  const objetosCarrito = JSON.parse(localStorage.getItem('item'));
+  objetosCarrito.forEach((zapato, index) => {
+    if (zapato.title === zapatoBorrar) {
+      objetosCarrito.splice(index, 1);
+    }
+  });
+  localStorage.setItem('item', JSON.stringify(objetosCarrito));
 };
 
 /* pasos a seguir:
